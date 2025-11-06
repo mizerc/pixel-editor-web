db673eb3ae112e55dcbf2db5bbf7ac6d958901b7
Mauricio Ize <mauriciodcjz@gmail.com>
2025-11-05 21:52:42 -0800

    Add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/pixel-editor/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist'
   }
 })
 
